ddeb5bdb29f8f91cc39a0e4e2792b476a9d0d32c
Kennedymuthuri <muthurikennedy082@gmail.com>
2023-07-18 14:34:01 +0300

    nav menu

--- messager.web/Shared/NavMenuBase.cs
+++ messager.web/Shared/NavMenuBase.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components;
 namespace messager.web.Shared;
 
 public class NavMenuBase:ComponentBase{
-    private messager.model.NavMenu[]? navMenus;
+    public messager.model.NavMenu[]? navMenus;
     protected override void OnInitialized()
     {
         navMenus = messager.web.Data.NavData.GetNavMenu();
