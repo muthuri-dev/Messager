6974f6eae14cdbad5e999fd9f2fe7ecb96c83574
Kennedymuthuri <muthurikennedy082@gmail.com>
2023-07-20 15:45:56 +0300

    nav bar functionality

--- messager.web/Data/NavData.cs
+++ messager.web/Data/NavData.cs
@@ -5,24 +5,26 @@ public class NavData{
     private static readonly List<NavMenu> navMenus = new(){
         new NavMenu(){
             Icon = "images/kenn.JPG",
+            Name = "Account"
         },
         new NavMenu(){
-            Icon =  "images/mess.svg"
+            Icon =  "images/mess.svg",
+            Name = "message"
         },
         new NavMenu(){
-            Icon =  "images/group.png"
+            Icon =  "images/group.png",
+            Name = "group"
         },
         new NavMenu(){
-            Icon =  "images/notif.svg"
+            Icon =  "images/notif.svg",
+            Name = "notification"
         },
         new NavMenu(){
-            Icon =  "images/sett.svg"
+            Icon =  "images/sett.svg",
+            Name = "settings"
         },
     };   
     public static NavMenu[] GetNavMenu(){
         return navMenus.ToArray();
     }
-    public void seting(){
-
-    } 
 }
